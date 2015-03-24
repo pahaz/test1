500754d77ce0de5294ced157f7b470b89e0401ca
Arthur Liss <artli@e1.ru>
2015-03-24 18:14:50 +0500

    add static files support; refactor

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from urllib.parse import parse_qs
 from warnings import warn
 from wsgiref.headers import Headers
 from wsgiref.util import request_uri
+import os.path
+import wsgi
 
 
 def parse_http_get_data(environ):
@@ -67,3 +69,16 @@ def get_first_element(dict_, key, default=None):
     if type(val) in (list, tuple) and len(val) > 0:
         val = val[0]
     return val
+
+
+def get_file_path(requested_uri, base_uri):
+    requested_path = os.path.abspath(os.path.relpath(requested_uri, wsgi.APPLICATION_BASE_DIR))
+    base_path = os.path.abspath(os.path.relpath(base_uri, wsgi.APPLICATION_BASE_DIR))
+    common_prefix = os.path.commonprefix([requested_path, base_path]) + os.sep
+    if not common_prefix.startswith(base_path + os.sep):
+        raise IOError()
+    return requested_path
+
+def get_file_contents(file_path):
+    with open(file_path, 'rb') as file:
+        return file.read()
--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,15 @@
 import wsgiref.validate
+import os
+import mimetypes
 
 from utils import parse_http_x_www_form_urlencoded_post_data, \
     get_first_element, parse_http_get_data, parse_http_headers, \
-    parse_http_content_type, parse_http_uri
+    parse_http_content_type, parse_http_uri, get_file_path, get_file_contents
 
 DEBUG = True
-STATIC_URL = '/static/'
-STATIC_ROOT = 'data'
+APPLICATION_BASE_DIR = r'D:\IT\Универ\PythonWeb\git\python_web_hw1'
+STATIC_BASE_URI = '/static/'
+STATIC_BASE_DIR = 'data/'
 
 data_messages = [
     b'Name: user<br>Message: hi!',
@@ -14,54 +17,68 @@ data_messages = [
 ]
 
 
-@wsgiref.validate.validator
-def application(environ, start_response):
-    # https://www.python.org/dev/peps/pep-3333/#environ-variables
-    REQUEST_METHOD = environ['REQUEST_METHOD']
-    CONTENT_TYPE, CONTENT_TYPE_KWARGS = parse_http_content_type(environ)
-    SERVER_PROTOCOL = environ['SERVER_PROTOCOL']
-    HEADERS = parse_http_headers(environ)
-    URI_PATH = environ['PATH_INFO']
-    URI_QUERY = environ['QUERY_STRING']
-    URI = parse_http_uri(environ)
-    POST = parse_http_x_www_form_urlencoded_post_data(environ)
-    GET = parse_http_get_data(environ)
+def error_app(environ, start_response, error_code):
+    statuses = {
+        403: '403 Forbidden',
+        404: '404 Not Found'
+    }
+    status = statuses.get(error_code, '500 Internal Server Error')
+    headers = [('Content-type', 'text/html; charset=utf-8')]
+    start_response(status, headers)
+    return [status.encode('utf-8')]
 
+
+def static_app(environ, start_response):
+    # print('STATIC FILE DETECTED!')
+    URI_PATH = environ['PATH_INFO']
+    requested_uri = os.path.join(STATIC_BASE_DIR, URI_PATH[len(STATIC_BASE_URI):])
+    try:
+        requested_path = get_file_path(requested_uri, STATIC_BASE_DIR)
+        content = get_file_contents(requested_path)
+    except IOError:
+        return error_app(environ, start_response, 403)
+    mime_type, encoding = mimetypes.guess_type(requested_path)
+    status = '200 OK'
+    headers = [('Content-type', mime_type or 'application/octet-stream')]
+    if encoding:
+        headers.append(('Content-encoding', encoding))
+    start_response(status, headers)
+    return [content]
+
+
+def messages_app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
 
-    if URI_PATH == '/favicon.ico':
-        status = '404 Not Found'
+    if environ['REQUEST_METHOD'] == 'POST':
+        post_data = parse_http_x_www_form_urlencoded_post_data(environ)
+        name = get_first_element(post_data, 'name', '')
+        message = get_first_element(post_data, 'message', '')
+        data_message_text = "Name: {0}<br>Message: {1}".format(name, message)
+        print(data_message_text)
+        data_message_bytes = data_message_text.encode('utf-8')
+        data_messages.append(data_message_bytes)
+
+        status = '303 See Other'
+        headers.append(('Location', '/'))
         start_response(status, headers)
         return [b'']
-
-    if URI_PATH.startswith(STATIC_URL):
-        print('STATIC FILE DETECTED!')
-
-    if DEBUG:
-        print("{REQUEST_METHOD} {URI_PATH}?{URI_QUERY} {SERVER_PROTOCOL}\n"
-              "CONTENT_TYPE: {CONTENT_TYPE}; {CONTENT_TYPE_KWARGS}\n"
-              "POST: {POST}\n"
-              "GET: {GET}\n"
-              ":HEADERS:\n{HEADERS}\n"
-              .format(**locals()))
 
     with open('main.html', 'rb') as f:
         template_bytes = f.read()
-
-    if REQUEST_METHOD == 'POST':
-        status = '303 See Other'
-        headers.append(('Location', '/'))
-        name = get_first_element(POST, 'name', '')
-        message = get_first_element(POST, 'message', '')
-        data_message_text = "Name: {0}<br>Message: {1}".format(name, message)
-        data_message_bytes = data_message_text.encode('utf-8')
-        data_messages.append(data_message_bytes)
-        start_response(status, headers)
-        return [b'']
 
     messages = b'<hr>'.join(data_messages)
     template_bytes = template_bytes.replace(b'{{messages}}', messages)
 
     start_response(status, headers)
     return [template_bytes]
+
+
+@wsgiref.validate.validator
+def application(environ, start_response):
+    request_path = environ['PATH_INFO']
+    if request_path.startswith(STATIC_BASE_URI):
+        return static_app(environ, start_response)
+    if request_path == '/':
+        return messages_app(environ, start_response)
+    return error_app(environ, start_response, 404)
