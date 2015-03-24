08c9dd20b90eb66d547c9ca0599b2264b15172b6
mrprovoker <groser95@gmail.com>
2015-03-24 20:35:19 +0500

    test

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,24 @@
+import os
 import wsgiref.validate
-from conteroller import index
+import cgi
 from router import Router
 
 from utils import parse_http_x_www_form_urlencoded_post_data, \
-    get_first_element, parse_http_get_data, parse_http_headers, \
+    parse_http_get_data, parse_http_headers, \
     parse_http_content_type, parse_http_uri
 
 DEBUG = True
 STATIC_URL = '/static/'
-STATIC_ROOT = 'data'
+STATIC_ROOT = 'data/'
 
-router = Router()
-router.add_route('/', index)
+data_messages = [
+    b'Name: user<br>Message: hi!',
+    b'Name: user<br>Message: hi!',
+]
 
 
 @wsgiref.validate.validator
 def application(environ, start_response):
-    # https://www.python.org/dev/peps/pep-3333/#environ-variables
     REQUEST_METHOD = environ['REQUEST_METHOD']
     CONTENT_TYPE, CONTENT_TYPE_KWARGS = parse_http_content_type(environ)
     SERVER_PROTOCOL = environ['SERVER_PROTOCOL']
@@ -27,13 +29,28 @@ def application(environ, start_response):
     POST = parse_http_x_www_form_urlencoded_post_data(environ)
     GET = parse_http_get_data(environ)
 
+    status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
 
-    controller_callback = router.resolve(URI_PATH)
-    status, body = controller_callback(REQUEST_METHOD, GET, POST, headers)
 
     if URI_PATH.startswith(STATIC_URL):
-        print('STATIC FILE DETECTED!')
+        URI_PATH = URI_PATH.replace(STATIC_URL, STATIC_ROOT)
+        URI_PATH = os.path.normpath(URI_PATH)
+        file = URI_PATH.split('\\')[-1]
+        for dirpath, dirnames, filenames in os.walk(STATIC_ROOT):
+            for filename in filenames:
+                if filename == file:
+                    with open(os.path.join(dirpath, filename), 'rb') as f:
+                        contentLogo = f.read()
+                    status = '200 OK'
+                    headers = [('Content-type', "application/octet-stream")]
+                    start_response(status, headers)
+                    return [contentLogo]
+
+        status = '404 Not Found'
+        start_response(status, headers)
+        return [b'']
+
 
     if DEBUG:
         print("{REQUEST_METHOD} {URI_PATH}?{URI_QUERY} {SERVER_PROTOCOL}\n"
@@ -43,5 +60,39 @@ def application(environ, start_response):
               ":HEADERS:\n{HEADERS}\n"
               .format(**locals()))
 
+    with open('main.html', 'rb') as f:
+        template_bytes = f.read()
+
+    if REQUEST_METHOD == 'POST':
+        status = '303 See Other'
+        headers.append(('Location', '/'))
+
+        POST = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ, keep_blank_values=True)
+
+        fileItem = POST['file']
+        if fileItem.filename:
+            fullname = os.path.join(STATIC_ROOT, fileItem.filename)
+            i = 0
+            path = fileItem.filename
+            while os.path.exists(fullname):  # Переименовываем если такой файл существует.
+                path = str(i) + path
+                fullname = os.path.join(STATIC_ROOT, path)
+                i += 1
+
+            with open(fullname, 'wb') as out:
+                out.write(fileItem.file.read())
+            print("File saved to: " + fullname)
+
+        name = POST['name'].value
+        message = POST['message'].value
+        data_message_text = "Name: {0}<br>Message: {1}".format(name, message)
+        data_message_bytes = data_message_text.encode('utf-8')
+        data_messages.append(data_message_bytes)
+        start_response(status, headers)
+        return [b'']
+
+    messages = b'<hr>'.join(data_messages)
+    template_bytes = template_bytes.replace(b'{{messages}}', messages)
+
     start_response(status, headers)
-    return [body]
+    return [template_bytes]
