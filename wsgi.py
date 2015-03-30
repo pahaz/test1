08dc5f814423b95c8489bff3559c95d85a90adda
mrprovoker <groser95@gmail.com>
2015-03-30 22:28:57 +0500

    hw1

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,25 @@
+import cgi
 import os
 import wsgiref.validate
-import cgi
-from router import Router
 
+from conteroller import index
+from router import Router
 from utils import parse_http_x_www_form_urlencoded_post_data, \
     parse_http_get_data, parse_http_headers, \
     parse_http_content_type, parse_http_uri
+
 
 DEBUG = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'data/'
 
-data_messages = [
-    b'Name: user<br>Message: hi!',
-    b'Name: user<br>Message: hi!',
-]
+router = Router()
+router.add_route('/', index)
 
 
 @wsgiref.validate.validator
 def application(environ, start_response):
+    # https://www.python.org/dev/peps/pep-3333/#environ-variables
     REQUEST_METHOD = environ['REQUEST_METHOD']
     CONTENT_TYPE, CONTENT_TYPE_KWARGS = parse_http_content_type(environ)
     SERVER_PROTOCOL = environ['SERVER_PROTOCOL']
@@ -26,31 +27,28 @@ def application(environ, start_response):
     URI_PATH = environ['PATH_INFO']
     URI_QUERY = environ['QUERY_STRING']
     URI = parse_http_uri(environ)
-    POST = parse_http_x_www_form_urlencoded_post_data(environ)
+    POST = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ, keep_blank_values=True)
     GET = parse_http_get_data(environ)
 
-    status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
 
+    controller_callback = router.resolve(URI_PATH)
+    status, body = controller_callback(REQUEST_METHOD, GET, POST, headers)
 
     if URI_PATH.startswith(STATIC_URL):
         URI_PATH = URI_PATH.replace(STATIC_URL, STATIC_ROOT)
         URI_PATH = os.path.normpath(URI_PATH)
-        file = URI_PATH.split('\\')[-1]
-        for dirpath, dirnames, filenames in os.walk(STATIC_ROOT):
-            for filename in filenames:
-                if filename == file:
-                    with open(os.path.join(dirpath, filename), 'rb') as f:
-                        contentLogo = f.read()
-                    status = '200 OK'
-                    headers = [('Content-type', "application/octet-stream")]
-                    start_response(status, headers)
-                    return [contentLogo]
+        if os.path.exists(URI_PATH):
+            with open(URI_PATH, 'rb') as f:
+                contentLogo = f.read()
+            status = '200 OK'
+            headers = [('Content-type', "application/octet-stream")]
+            start_response(status, headers)
+            return [contentLogo]
 
         status = '404 Not Found'
         start_response(status, headers)
         return [b'']
-
 
     if DEBUG:
         print("{REQUEST_METHOD} {URI_PATH}?{URI_QUERY} {SERVER_PROTOCOL}\n"
@@ -60,39 +58,5 @@ def application(environ, start_response):
               ":HEADERS:\n{HEADERS}\n"
               .format(**locals()))
 
-    with open('main.html', 'rb') as f:
-        template_bytes = f.read()
-
-    if REQUEST_METHOD == 'POST':
-        status = '303 See Other'
-        headers.append(('Location', '/'))
-
-        POST = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ, keep_blank_values=True)
-
-        fileItem = POST['file']
-        if fileItem.filename:
-            fullname = os.path.join(STATIC_ROOT, fileItem.filename)
-            i = 0
-            path = fileItem.filename
-            while os.path.exists(fullname):  # Переименовываем если такой файл существует.
-                path = str(i) + path
-                fullname = os.path.join(STATIC_ROOT, path)
-                i += 1
-
-            with open(fullname, 'wb') as out:
-                out.write(fileItem.file.read())
-            print("File saved to: " + fullname)
-
-        name = POST['name'].value
-        message = POST['message'].value
-        data_message_text = "Name: {0}<br>Message: {1}".format(name, message)
-        data_message_bytes = data_message_text.encode('utf-8')
-        data_messages.append(data_message_bytes)
-        start_response(status, headers)
-        return [b'']
-
-    messages = b'<hr>'.join(data_messages)
-    template_bytes = template_bytes.replace(b'{{messages}}', messages)
-
     start_response(status, headers)
-    return [template_bytes]
+    return [body]
